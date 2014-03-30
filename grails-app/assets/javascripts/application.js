c705507dd2cafc8d8b08e4daebec9e3ba5b7cdb6
Mamadou Lamine DIARRA <msl.diarra@gmail.com>
2014-03-30 19:00:22 +0200

    Initial commit to keep track of original files

---
+++ grails-app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+//= require jquery
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require ./cafeblons
+
+// for more details see: http://emberjs.com/guides/application/
+Cafeblons = Ember.Application.create();
+
+
---
+++ grails-app/assets/javascripts/store.js.coffee
@@ -0,0 +1,11 @@
+# http://emberjs.com/guides/models/#toc_store
+# http://emberjs.com/guides/models/pushing-records-into-the-store/
+
+Cafeblons.Store = DS.Store.extend({
+
+})
+
+
+Cafeblons.ApplicationAdapter = DS.RESTAdapter.extend({
+
+})
